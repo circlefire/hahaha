c649f59087df1babc7bf0b29e511541e6b8e535a
circlefire <jwy0148@naver.com>
2023-02-03 16:11:38 +0900

    배열공부

---
+++ 2023_0203/2023_0203/01_array.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+int main() {
+	std::string friend1 = "全辨悼";
+	std::string friend2 = "己冕氢";
+
+	std::string friends[2];
+
+	friends[0] = "全辨悼";
+	friends[1] = "己冕氢";
+	friends[1] = "内爹柯";
+
+	/*std::string friends[] = {"全辨悼", "己冕氢"};
+	std::cout << friends[1];*/
+
+	//int nums[3] = { 1,2,3 };
+	//std::cout << nums[2];
+	double nums[3] = { 1.5,2.5,3.5 };
+
+	std::string fruit[2][3] = { { "apple","grape",""},{"orange","banana",""} };
+
+}
