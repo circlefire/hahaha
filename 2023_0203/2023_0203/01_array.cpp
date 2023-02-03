c50d968844aa8c98b9401b76aaafc8bec31a47a5
circlefire <jwy0148@naver.com>
2023-02-03 17:27:44 +0900

    d

--- 2023_0203/2023_0203/01_array.cpp
+++ 2023_0203/2023_0203/01_array.cpp
@@ -4,11 +4,11 @@ int main() {
 	std::string friend1 = "홍길동";
 	std::string friend2 = "성춘향";
 
-	std::string friends[2];
+	std::string friends[3];
 
 	friends[0] = "홍길동";
 	friends[1] = "성춘향";
-	friends[1] = "코딩온";
+	friends[2] = "코딩온";
 
 	/*std::string friends[] = {"홍길동", "성춘향"};
 	std::cout << friends[1];*/
@@ -17,6 +17,25 @@ int main() {
 	//std::cout << nums[2];
 	double nums[3] = { 1.5,2.5,3.5 };
 
-	std::string fruit[2][3] = { { "apple","grape",""},{"orange","banana",""} };
+	std::string fruit[2][3] = {
+		{"faas","aaa","wdw"},{"asdw","wrsd","hdrt" }
+	};
 
+	std::string fruits[2][3][2] = {
+		{{"",""},{"aaa",""},{"",""}},{{"",""},{"",""},{"",""}}
+	};
+	std::cout << fruits[0][1][0] << std::endl;
+	std::cout << fruits << std::endl; // 주소값 출력.
+
+	for (int i = 0; i < 3; i++) {
+		std::cout << friends[i] << std::endl;
+	}
+	for (std::string f:friends) { // for each 문
+		std::cout << f << std::endl;
+	}
+	for (auto &f : fruit) { // 2차원 for each 문
+		for (auto f2 : f) {
+			std::cout << f2 << std::endl;
+		}
+	}
 }
