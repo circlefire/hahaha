56cbd80126ffb31b29c998ce743b6b83d3217842
circlefire <jwy0148@naver.com>
2023-02-08 01:46:45 +0900

    Jjajeung

--- 0207_lecture/0207_lecture/filewrite.cpp
+++ 0207_lecture/0207_lecture/filewrite.cpp
@@ -2,53 +2,46 @@
 #include<fstream>
 #include<string>
 using namespace std;
+
 int main() {
-	string pw, name, phonenum, str, str2;
-	string line = " ";
-	string file_content = "";
-	ifstream file_r;
-	ofstream file("member.txt", ios::out | ios::app);
-	ofstream file2("member_tel.txt");
-	cout << "이름을 입력하세요: ";
-	cin >> name;
-	cout << "비번을 입력하세요: ";
-	cin >> pw;
+	string pw, name, phonenum, str, str2, line, file_content = "";
+	ifstream file_r, file_r2; 
+	file << name << " " << phonenum;
 	file_r.open("member.txt");
-	if (file_r.fail()) {
+	file_r2.open("member_tel.txt");
+
+	if (file_r.fail()||file_r2.fail()) {
 		cout << "파일 없음" << endl;
 		return -1;
 	}
-	while (getline(file_r, line)) {
-		int i = 0;
-		i += 1;
-		if (line == name + " " + pw) {
-			cout << "로그인 성공" << endl;
-			cout << "전화번호를 입력해주세요: ";
-			cin >> phonenum;
-
-			if (file2.fail()) {
-				cout << "파일 없음" << endl;
-				return -1;
+	
+	for (int i = 0; i < 3 ; i++) {
+		cout << "이름을 입력하세요: ";
+		cin >> name;
+		cout << "비번을 입력하세요: ";
+		cin >> pw;
+		while(getline(file_r, line)) {
+			if (line == name + " " + pw) {
+				cout << "로그인 성공" << endl;
+				cout << "전화번호를 입력해주세요: ";
+				cin >> phonenum;
+				i = 3;
+				break;
 			}
-
-
-			ifstream file_R("member_tel.txt");
-			while (file_R >> str >> str2) {
-				int j = 0;
-				j += 1;
-				if (str == name) {
-					file_content += name + " " + phonenum + "\n";
-				}
-				else file_content += str + " " + str2 + "\n";
-			}
-			file2 << file_content;
-
-			break;
 		}
-		else if (i == 4) {
-			cout << "로그인 실패";
-		}
-
+		if (i == 3)break;
+		cout << "로그인 실패" << endl;
 	}
-	file_r.close(); // file 종료
+	int i = 0;
+	while (file_r2 >> str >> str2) {
+		i += 1;
+		if (str != name) {
+			file_content += str + " " + str2;
+		}
+		else if (str == name) {
+			file_content += name + " " + phonenum;
+		}
+	}
+	ofstream file("member_tel.txt");
+	file << name << " " << phonenum;
 }
