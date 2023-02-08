224d5f42ecb04140e3d1986eb594896a86baf88d
circlefire <jwy0148@naver.com>
2023-02-08 17:54:50 +0900

    class_prac2

--- 0208/0208/prac1.cpp
+++ 0208/0208/prac1.cpp
@@ -1,23 +1,67 @@
 #include<iostream>
 using namespace std;
 
-class Rectangle {
-	double width, height;
+class Character {
 public:
-	Rectangle(double width, double height) {
-		this->width = width;
-		this->height = height;
+	string name;
+	int level;
+	int item;
+	Character(string name) {
+		this->name = name;
+		level = 0;
+		item = 0;
 	}
-	double area() {
-		return width * height;
+	void setName(string name) {
+		this->name = name;
+	}
+	void levelUp() {
+		level += 1;
+	}
+	void itemUp() {
+		item += 1;
+	}
+	void itemDown() {
+		item -= 1;
 	}
 };
 
 int main() {
-	double width=0, height=0;
-	cout << "width와 height 입력: ";
-	cin >> width >> height;
-	Rectangle rect(width, height);
-	cout << "넓이: " << rect.area();
+	int num=1;
+	string name;
+	cout << "캐릭터 이름을 입력하세요: ";
+	cin >> name;
+	Character cha(name);
 
+	while (num!=0) {
+		cout << "무슨 조작을 하시겠습니까?(0~4): ";
+		cin >> num;
+		if (num == 1) {
+			cout << "이름 변경: ";
+			cin >> name;
+			cha.setName(name);
+			cout << "이름: " << cha.name << endl;
+		}
+		else if (num == 2) {
+			cout << "level up!" << endl;
+			cha.levelUp();
+		}
+		else if (num == 3) {
+			cout << "item 줍줍 " << endl;
+			cha.itemUp();
+		}
+		else if (num == 4) {
+			cout << "item 사용" << endl;
+			cha.itemDown();
+		}
+		else if (num == 5) {
+			cout << "name: " << cha.name << endl << "level: " << cha.level << endl << "item: " << cha.item << endl;
+		}
+		else if (num == 0) {
+			cout << "게임 종료";
+			break;
+		}
+
+	}
+
+	return 0;
 }
