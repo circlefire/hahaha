bd6a0cab10b5ad95a4056204fff7ff90b2942aef
circlefire <jwy0148@naver.com>
2023-02-15 15:42:32 +0900

    timeattack

---
+++ 0215/0215/timeattack.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<ctime>
+#include<string>
+using namespace std;
+
+int main() {
+	clock_t startTime = clock();
+	string word, nextword, line;
+	int n, wordnum=0;
+	
+	word = "airplane";
+	line = "airplane";
+	cout << "airplane\n";
+	while (1) {
+		clock_t endTime = clock();
+		if (((endTime - startTime) / CLOCKS_PER_SEC) >= 10) {
+			cout << "타임 오버!\n\n게임 종료!\n총 입력한 단어 개수 : " << wordnum;
+			break;
+		}
+
+		cout << "다음 단어를 입력하세요: ";
+
+		cin >> nextword;
+
+		n = word.size();
+		if (word[n - 1] != nextword[0]) {
+			cout << "잘못된 입력입니다.\n";
+		}
+		else {
+			line += "->" + nextword;
+			cout << line << endl;
+			wordnum++;
+			word = nextword;
+		}
+		cout << "\n";
+	}
+
+
+}
