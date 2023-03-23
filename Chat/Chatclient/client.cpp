7bf554962705e8f0dab52d76f376526ce4d9ebd8
circlefire <jwy0148@naver.com>
2023-03-23 15:43:41 +0900

    통신실습

---
+++ Chat/Chatclient/client.cpp
@@ -0,0 +1,74 @@
+#pragma comment(lib, "ws2_32.lib")
+
+#include <WinSock2.h>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <sstream>
+#include <WS2tcpip.h>
+
+#define MAX_SIZE 1024
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+SOCKET client_sock;
+string my_nick;
+
+int chat_recv() {
+	char buf[MAX_SIZE] = {};
+	string msg;
+	while (1) {
+		ZeroMemory(&buf, MAX_SIZE);
+		if (recv(client_sock, buf, MAX_SIZE, 0) > 0) {
+			msg = buf;
+			string user;
+			std::stringstream ss(msg);
+			ss >> user;
+			if (user != my_nick) cout << buf << endl;
+		}
+		else {
+			cout << "Server Off!" << endl;
+			return -1;
+		}
+	}
+}
+
+int main() {
+	WSADATA wsa;
+	int code = WSAStartup(MAKEWORD(2, 2), &wsa);
+	if (!code) {
+		cout << " 사용할 닉네임 입력 >> ";
+		cin >> my_nick;
+
+		client_sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+		SOCKADDR_IN client_addr = {};
+		client_addr.sin_family = AF_INET;
+		client_addr.sin_port = htons(7777);
+		InetPton(AF_INET, TEXT("127.0.0.1"), &client_addr.sin_addr);
+
+		while (1) {
+			if (!connect(client_sock,(SOCKADDR*)&client_addr,sizeof(client_addr))) {
+				cout << "Server Connect" << endl;
+				send(client_sock, my_nick.c_str(), my_nick.length(), 0);
+				break;
+			}
+			cout << "connecting..." << endl;
+		}
+		std::thread th2(chat_recv);
+
+		while (1) {
+			string text;
+			std::getline(cin, text);
+			const char* buffer = text.c_str();
+			send(client_sock, buffer, strlen(buffer), 0);
+		}
+		th2.join();
+		closesocket(client_sock);
+	}
+	WSACleanup();
+	return 0;
+}
