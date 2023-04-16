cd31c2e738a93654a286f1b1bffa77cd94d1a1fd
circlefire <jwy0148@naver.com>
2023-04-16 19:22:13 +0900

    for문 쉽게 사용

--- coteprac/coteprac/push_back활용.cpp
+++ coteprac/coteprac/push_back활용.cpp
@@ -1,18 +1,14 @@
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
 string solution(string rsp) {
     string answer = "";
-    int irsp = stoi(rsp);
-    while (1) {
-        if (irsp % 10 == 2) answer.insert(0, "0");
-        else if (irsp % 10 == 0) answer.insert(0, "5");
-        else if (irsp % 10 == 5) answer.insert(0, "2");
-        if (irsp < 10) return answer;
-        irsp /= 10;
+    for (char c : rsp) {
+        if (c == '2') answer += '0';
+        else if (c == '0') answer += '5';
+        else if (c == '5') answer += '2';
     }
     return answer;
 }
